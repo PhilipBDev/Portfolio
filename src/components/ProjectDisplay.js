8e7eb530d191cd3e128845ae878006c234ef66d6
Philip <philipbdev@gmail.com>
2021-10-10 19:25:56 -0400

    Mobile CSS

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -123,6 +123,11 @@ const SingleProjectDiv = styled.div`
     width: 90%;
     height: 90%;
   }
+
+  @media (max-width: 350px) {
+    width: 100%;
+    height: 100%;
+  }
 `
 const ProjectDiv = styled.div`
   display: grid;
