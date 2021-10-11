98ed2ef20b2d05d028c2e5adc86509bb0de2e15d
Philip <philipbdev@gmail.com>
2021-10-11 08:23:53 -0400

    Mobile CSS

--- src/html.js
+++ src/html.js
@@ -7,6 +7,7 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, shrink-to-fit=no" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
