b470630e734d76fa62337f2e08d3cc2b9bd6d440
Philip <PhilipBDev@gmail.com>
2021-10-31 02:54:44 -0400

    Updating portfolio progress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,8 +22,6 @@ const Flex = styled.footer`
   align-items: center;
   justify-content: center;
   background-image: linear-gradient(#2a9df4, #187bcd);
-  border-bottom-left-radius: 50% 20%;
-  border-bottom-right-radius: 50% 20%;
 `
 const FooterText = styled.p`
   font-weight: 600;
