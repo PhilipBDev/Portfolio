c1213df3aa4e9bc127fd72804cdd9a8fffd068c3
Philip <PhilipBDev@gmail.com>
2021-10-30 22:34:27 -0400

    Updating portfolio progress

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,14 +7,11 @@ import { createGlobalStyle } from "styled-components"
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
 html,body {
+    min-height: 100vh;
     padding: 0;
-    color: #000;
-    font-size: 12px;
-    height: 100%;
-    width: 100%;
-    overflow-x: hidden;
-    background-color: #fff;
-    // background-image: linear-gradient(315deg, #6a93cb 0%, #a4bfef 74%);
+    color: #fff;
+    font-size: 16px;
+    max-width: 100%;
 
     // @media (max-width: 850px) {
     //   background-image: none;
